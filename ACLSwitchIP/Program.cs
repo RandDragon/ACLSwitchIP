2e78833628830783f4adef3a4359c1fbfc9ee800
RandDragon <RandDragon6@gmail.com>
2017-05-18 11:49:27 +0300

    Первая версия.

--- ACLSwitchIP/Program.cs
+++ ACLSwitchIP/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,34 @@ namespace ACLSwitchIP
     {
         static void Main(string[] args)
         {
+            List<Client> ClientList = new List<Client>();
+            StreamReader sr = new StreamReader("LoadDB.csv", Encoding.GetEncoding(1251));
+            sr.ReadLine();
+
+            while (!sr.EndOfStream)
+            {
+                try
+                {
+
+                    string[] str = sr.ReadLine().Split(';');
+
+                    String[] port = str[0].Split('@');
+                    int numport;
+
+                    int.TryParse(port[0], out numport);
+                    Client client = new Client(numport, port[1], str[1]);
+                    ClientList.Add(client);
+
+                }
+                catch (Exception)
+                { }
+            }
+
+            foreach (Client client in ClientList)
+            {
+                if (client.SwitchName == "br13-des2.vs.bcitelecom.ru") Console.WriteLine(ActionACL.addACL(client));
+            }
+            Console.ReadLine();
         }
     }
 }
